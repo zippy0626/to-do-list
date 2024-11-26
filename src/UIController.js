8434f706393b3d3579ad9bd2ef49b0c88b9590c7
zippy0626 <zippy0626@gmail.com>
2024-11-26 09:27:06 -0500

    added handlers for task buttons, Sort by button, and show all projects methods

--- src/UIController.js
+++ src/UIController.js
@@ -11,14 +11,14 @@ const Controller = {
     const relatedTasksContainer = document.querySelector('.group-related-tasks-wrapper');
 
     //show today's tasks
-    this.showAllTasks();
+    this.showTodayTasks();
     
     //show default editor
     const editor = document.querySelector('#editor');
     editor.innerHTML = getEditorAs("default")
 
     //show all projects
-
+    this.showAllProjects();
   },
 
   handleTaskButtons() {
@@ -42,12 +42,38 @@ const Controller = {
       }
 
       //handle past projects/tasks button
+
     });
   },
 
-  showAllTasks() {
-    let today = format(new Date(), "MM-dd-yyyy");
+  handleSortByButton() {
+    const sortByButton = document.querySelector('.sort-by-button');
+    const sortOptions = document.querySelector('.sort-options');
+    
+    sortByButton.addEventListener('click', () => {
+      sortOptions.classList.toggle("hidden")
+    });
 
+    sortOptions.addEventListener('click', (e)=>{
+      let targetText = e.target.textContent;
+      
+      if (targetText==="Lowest Priority to Highest Priority") {
+        
+      }
+      if (targetText==="Highest Priority to Lowest Priority") {
+        
+      }
+      if (targetText==="Date Created") {
+        
+      }
+      if (targetText==="Date Due") {
+        
+      }
+    });
+  },
+
+  //when to use?
+  showAllTasks() {
     const mainDisplay = document.querySelector('#main-display');
     mainDisplay.innerHTML = getMainDisplayAs("todaysTasks");
     const cardContainer = document.querySelector('.card-container');
@@ -66,22 +92,28 @@ const Controller = {
   showTodayTasks() {
     let today = format(new Date(), "MM-dd-yyyy");
 
-    const mainDisplay = document.querySelector('#main-display');
-    mainDisplay.innerHTML = getMainDisplayAs("todaysTasks");
-    const cardContainer = document.querySelector('.card-container');
-    cardContainer.innerHTML = "";
-
     if (!localStorage.length) {
-      //handle no tasks or projects
+      const mainDisplay = document.querySelector('#main-display');
+      mainDisplay.innerHTML = getMainDisplayAs("noTodays");
+      return;
     }
+
+    const mainDisplay = document.querySelector('#main-display');
+      mainDisplay.innerHTML = getMainDisplayAs("todaysTasks");
+    const cardContainer = document.querySelector('.card-container');
+      cardContainer.innerHTML = "";
 
     for (let i = 0; i < localStorage.length; i++) {
       let taskObj = Storage.getItem(Storage.getKey(i));
       const taskCreateDate = taskObj._createDate;
       const taskDueDate = taskObj._dueDate;
+      const taskIsComplete = taskObj._isComplete;
 
       if (
-        taskCreateDate===date
+        taskCreateDate===today || 
+        taskDueDate===today || 
+        (taskDueDate < today && !taskIsComplete) ||
+        (taskDueDate > today && !taskIsComplete)
       ) {
         const taskCard = makeCardFor("cardContainer", taskObj);
         cardContainer.insertAdjacentHTML("beforeend", taskCard);
@@ -95,21 +127,54 @@ const Controller = {
     const mainDisplay = document.querySelector('#main-display');
     mainDisplay.innerHTML = getMainDisplayAs("upcomingTasks");
     const cardContainer = document.querySelector('.card-container');
-    cardContainer.innerHTML = '';
+    cardContainer.innerHTML = "";
 
     for (let i = 0; i < localStorage.length; i++) {
       let taskObj = Storage.getItem(Storage.getKey(i));
+      const taskDueDate = taskObj._dueDate;
+      const taskCreateDate = taskObj._createDate;
       
-      if (!taskObj._isComplete) {
+      if (
+        taskCreateDate === today ||
+        taskDueDate > today || 
+        taskDueDate === today
+      ) {
         const taskCard = makeCardFor("cardContainer", taskObj);
         cardContainer.insertAdjacentHTML("beforeend", taskCard);
       }
     };
+
+    if (!cardContainer.innerHTML) {
+      mainDisplay.innerHTML = getMainDisplayAs("noUpcoming")
+    }
   },
 
   showAllProjects() {
     const projectContainer = document.querySelector('.project-container');
+    projectContainer.innerHTML = "";
 
+    for (let i = 0; i < localStorage.length; i++) {
+      let project = Storage.getItem(Storage.getKey(i));
+
+      if (project && project._isProject) {
+        const projectCard = makeCardFor("projectContainer", project);
+        projectContainer.insertAdjacentHTML("beforeend", projectCard);
+      };
+    };
+  },
+
+  sortAllProjects(sortOption) {
+    //lowToHi, highToLow, dateCreated, dateDue
+    const projectContainer = document.querySelector('.project-container');
+    projectContainer.innerHTML = "";
+
+    for (let i = 0; i < localStorage.length; i++) {
+      let project = Storage.getItem(Storage.getKey(i));
+
+      if (project && project._isProject) {
+        
+      };
+    };
   }
 }
 
