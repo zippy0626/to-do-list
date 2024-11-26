a04df373f60026f218b996ad10592e26163e2e88
zippy0626 <zippy0626@gmail.com>
2024-11-26 09:30:14 -0500

    addedhandling for no Tasks/no Upcoming Tasks

--- src/MainDisplay.js
+++ src/MainDisplay.js
@@ -3,7 +3,7 @@ import { format } from "date-fns";
 /**
  * Generates HTML templates for different main display types in the app.
  *
- * @param {"todaysTasks" | "upcomingTasks" | "pastTasksProjects"} displayType - The type of display to render. 
+ * @param {"todaysTasks" | "upcomingTasks" | "pastTasksProjects" | "noTodays" | "noUpcoming" | "noPastTasksProjects"} displayType - The type of display to render. 
  *   - "todaysTasks": Shows tasks for the current day.
  *   - "upcomingTasks": Shows tasks scheduled for future dates.
  *   - "pastTasksProjects": Displays tasks and projects from the past.
@@ -71,6 +71,53 @@ export default function getMainDisplayAs(displayType) {
       <!-- load stuff here -->
       <div class="card-container">
 
+      </div>
+    </div>
+    `;
+    return str;
+  }
+  if (displayType==="noTodays") {
+    const str = `
+    <div class="main-title-date-wrapper">
+      <h1 class="main-title">
+        Today's Tasks
+      </h1>
+      <h2 class="main-date">
+        ${dateToday}
+      </h2>
+    </div>
+
+    <div class="card-container-wrapper">
+      <!-- load stuff here -->
+      <div class="card-container">
+        <div class="message-wrapper">
+          <div class="message">
+            No tasks found/due for today.
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+  }
+  if (displayType==="noUpcoming") {
+    const str = `
+      <div class="main-title-date-wrapper">
+      <h1 class="main-title">
+        Upcoming Tasks
+      </h1>
+      <h2 class="main-date">
+        ${dateToday}
+      </h2>
+    </div>
+
+    <div class="card-container-wrapper">
+      <!-- load stuff here -->
+      <div class="card-container">
+        <div class="message-wrapper">
+          <div class="message">
+            No upcoming tasks found/due in future.
+          </div>
+        </div>
       </div>
     </div>
     `;
