27c5bd9dc3ae39aeac3ea93bbd0b93d7fd3715f7
zippy0626 <zippy0626@gmail.com>
2024-11-28 11:15:44 -0500

    removed getter/setters, remove private undescore variables

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -2,83 +2,22 @@ import { format } from "date-fns";
 import { addDays } from "date-fns";
 
 export default class toDoItem {
-  //default values
   constructor(
     title = "Untitled",
     description = "No Description",
-    dueDate = format(addDays(new Date(), 7), "MM-dd-yyyy"),
+    dueDate = format(addDays(new Date(), 7), "MM-dd-yyyy"), //default 7 days
     priority = "Medium",
-    checkList = [], // {name: ,desc: ,isComplete: ,}
+    checkList = [], // {name: , isComplete: ,}
     isComplete = false,
-    projectTitle = null //not linked to a project
+    projectTitleLink = null //not linked to a project
   ) {
-    this._title = title;
-    this._description = description;
-    this._createDate = format(new Date(), "MM-dd-yyyy");
-    this._dueDate = dueDate;
-    this._priority = priority; //critical, high, medium, low
-    this._checkList = checkList;
-    this._isComplete = isComplete;
-    this._projectTitle = projectTitle;
-  }
-
-  get title() {
-    return this._title;
-  }
-  set title(newTitle) {
-    if (typeof newTitle !== "string" || newTitle.trim() === "") {
-      throw new Error("Title is invalid");
-    }
-    this._title = newTitle.trim();
-  }
-
-  get description() {
-    return this._description;
-  }
-  set description(newDescription) {
-    if (typeof newDescription !== "string" || newDescription.trim() === "") {
-      throw new Error("Description is invalid");
-    }
-    this._description = newDescription;
-  }
-
-  get createDate() {
-   return this._createDate; 
-  }
-
-  get dueDate() {
-    return this._dueDate;
-  }
-  set dueDate(newDueDate) {
-    this._dueDate = newDueDate;
-  }
-
-  get priority() {
-    return this._priority;
-  }
-  set priority(newPriority) {
-    this._priority = newPriority;
-  }
-
-  get checkList() {
-    return this._checkList;
-  }
-
-  get isComplete() {
-    return this._isComplete;
-  }
-  set isComplete(bool) {
-    this._isComplete = bool;
-  }
-
-  get projectTitle() {
-    return this._projectTitle;
-  }
-  set projectTitle(newProjectTitle) {
-    if (!newProjectTitle) {
-      this._projectTitle = null;
-      return;
-    }
-    this._projectTitle = newProjectTitle.trim();
+    this.title = title;
+    this.description = description;
+    this.createDate = format(new Date(), "MM-dd-yyyy");
+    this.dueDate = dueDate;
+    this.priority = priority; //critical, high, medium, low
+    this.checkList = checkList;
+    this.isComplete = isComplete;
+    this.projectTitleLink = projectTitleLink;
   }
 }
