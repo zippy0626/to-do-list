0e758a12223d53e2e91c7a178a387a75ce16701a
zippy0626 <zippy0626@gmail.com>
2024-11-28 16:45:57 -0500

    remove 'private' variable

--- src/Project.js
+++ src/Project.js
@@ -9,74 +9,31 @@ export default class Project {
     dueDate = format(addDays(new Date(), 14), "MM-dd-yyyy"),
     priority = "Medium",
     isComplete = false,
-    itemContainer = []
   ) {
-    this._title = title;
-    this._description = description;
-    this._createDate = format(new Date(), "MM-dd-yyyy");
-    this._dueDate = dueDate;
-    this._priority = priority; //critical, high, medium, low
-    this._isComplete = isComplete;
-    this._itemContainer = itemContainer;
+    this.title = title;
+    this.description = description;
+    this.createDate = format(new Date(), "MM-dd-yyyy");
+    this.dueDate = dueDate;
+    this.priority = priority; //critical, high, medium, low
+    this.isComplete = isComplete;
+    this.isProject = true;
+    this.taskContainer = []; // {name: , isComplete: ,}
   }
 
-  get title() {
-    return this._title;
-  }
-  set title(newTitle) {
-    if (typeof newTitle !== "string" || newTitle.trim() === "") {
-      throw new Error("Title cannot be Empty!");
+  add(task) {
+    if (this.taskContainer.includes(task)) {
+      throw new Error("Item is already in the container!");
     }
-    this._title = newTitle.trim();
+    task.projectTitle = this.title;
+    this.taskContainer.push(task);
   }
-
-  get description() {
-    return this._description;
-  }
-  set description(newDescription) {
-    if (typeof newDescription !== "string" || newDescription.trim() === "") {
-      throw new Error("Description cannot be Empty!");
+  remove(task) {
+    const index = this.taskContainer.indexOf(task);
+    if (index === -1) {
+      throw new Error("Item not found in the container!");
     }
-    this._description = newDescription.trim();
-  }
 
-  get createDate() {
-    return this._createDate;
-  }
-
-  get dueDate() {
-    return this._dueDate;
-  }
-  set dueDate(newDueDate) {
-    this._dueDate = newDueDate;
-  }
-
-  get priority() {
-    return this._priority;
-  }
-  set priority(newPriority) {
-    this._priority = newPriority;
-  }
-
-  get isComplete() {
-    return this._isComplete;
-  }
-  set isComplete(bool) {
-    this._isComplete = bool;
-  }
-
-  get itemContainer() {
-    return this._itemContainer;
-  }
-
-  add(Item) {
-    Item.projectID = this._title;
-    this._itemContainer.push(Item);
-  }
-  remove(Item) {
-    Item.projectID = null;
-    this._itemContainer = this._itemContainer.filter(
-      (eachItem) => eachItem !== Item
-    );
+    task.projectTitle = null;
+    this.itemContainer.splice(index, 1);
   }
 }
