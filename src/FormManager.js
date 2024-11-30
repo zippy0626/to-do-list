b32df2ab5d9e2d6ecdce49c559e9725279d3c97b
zippy0626 <zippy0626@gmail.com>
2024-11-30 15:38:58 -0500

    added update form when clicking a project

--- src/FormManager.js
+++ src/FormManager.js
@@ -1,3 +1,4 @@
+import makeCardFor from "./CardMaker";
 import getEditorAs from "./Editor";
 
 const FormManager = {
@@ -16,8 +17,8 @@ const FormManager = {
     const isComplete = form.querySelector("#task-is-complete");
     const isNotComplete = form.querySelector("#task-is-not-complete");
       task.isComplete ? 
-        isComplete.checked = true : 
-        isNotComplete.checked = true;
+      isComplete.checked = true : 
+      isNotComplete.checked = true;
     
     const descriptionField = form.querySelector("#task-desc");
       descriptionField.value = task.description;
@@ -45,7 +46,7 @@ const FormManager = {
       const subTasks = task.checkList; //array of dictionaries
       if (subTasks) {
         for (const task of subTasks) {
-          checkListField.value += task.description + ", "
+          checkListField.value += task.name + ", \n"
         }
       }
 
@@ -60,7 +61,65 @@ const FormManager = {
     editor.innerHTML = getEditorAs("viewEditProject");
     
     const form = editor.querySelector("form");
-  }
+
+    const taskTitleField = form.querySelector("#task-title")
+      taskTitleField.value = project.title;
+    
+    const isComplete = form.querySelector("#task-is-complete");
+    const isNotComplete = form.querySelector("#task-is-not-complete");
+      project.isComplete ? 
+      isComplete.checked = true : 
+      isNotComplete.checked = true;
+    
+    const descriptionField = form.querySelector("#task-desc");
+      descriptionField.value = project.description;
+    
+    const dueDateField = form.querySelector("#task-due-date");
+      let [month, day, year] = project.dueDate.split("-");
+      dueDateField.value = `${year}-${month}-${day}`;
+    
+    const priorityField = form.querySelector(".priority-radio-buttons-wrapper");
+      const [low, medium, high, critical] = priorityField.querySelectorAll("input");
+      if (project.priority==="Low") {
+        low.checked = true;
+      }
+      if (project.priority==="Medium") {
+        medium.checked = true;
+      }
+      if (project.priority==="High") {
+        high.checked = true;
+      }
+      if (project.priority==="Critical") {
+        critical.checked = true;
+      }
+
+    //handle taskContainer/subtasks/related tasks
+    const relatedTasksContainer = form.querySelector(".group-related-tasks-wrapper");
+    if (!project.taskContainer.length) {
+      relatedTasksContainer.innerHTML = `
+      <div class="message-wrapper">
+        <div class="message">No subtasks found.</div>
+      </div>
+      `
+    }
+    for (const subTask of project.taskContainer) {
+      let tempName = subTask.name.replace(/\s+/g, "-");
+      
+      const card = makeCardFor("relatedTasksContainer", {name: tempName, ...subTask})
+      relatedTasksContainer.insertAdjacentHTML("beforeend", card);
+
+      const addedCardCheckBox = relatedTasksContainer.querySelector(`#${tempName}`);
+      if (subTask.isComplete) addedCardCheckBox.checked = true;
+    }
+  },
+
+  handleAddNewTaskorProjectForm(option) {//task or project
+
+  },
+
+  handleEditTaskorProjectForm(option) {//task or project
+    
+  },
 };
 
 export default FormManager;
